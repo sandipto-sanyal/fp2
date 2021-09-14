2a6f1a0af8b4df9be896b57bec63cb1ab7b8f86b
sandipto-sanyal <44098866+sandipto-sanyal@users.noreply.github.com>
2021-09-14 09:18:10 +0530

    Create train-local.sh

---
+++ train-local.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# This scripts performs local training for a TensorFlow model.
+
+set -ev
+
+echo "Training local ML model"
+
+
+
+PACKAGE_PATH=./trainer
+
+export EPOCHS=10
+export EVAL_STEPS=100
+export DF_PATH=gs://foundation_project2/training_folder/zomato.csv
+
+gcloud ai-platform local train \
+        --module-name=trainer.task \
+        --package-path=${PACKAGE_PATH} \
+        -- \
+        --df-path="$DF_PATH" \
+        --epochs=$EPOCHS \
+        --eval-steps=$EVAL_STEPS \
